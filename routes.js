06b59314a1ba9f88eae2b02f037bba4d66cdc348
Vikas Kad <vikas@tradala.in>
2018-10-01 13:03:42 +0530

    added bodyparser

--- routes.js
+++ routes.js
@@ -3,8 +3,14 @@
  */
 
 'use strict';
+var bodyParser = require('body-parser')
 
 module.exports = function (app) {
+
+  app.use(bodyParser.json()); // support json encoded bodies
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
   app.use('/api/user', require('./apis/users'));
   app.use('/api/order', require('./apis/orders'));
 
